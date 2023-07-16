7edc3be9737a5f78e1d9d731db0b803be6ce96e4
meselghea <122159476+meselghea@users.noreply.github.com>
2023-07-17 04:52:40 +0700

    update

--- src copy/menu-images/dto/update-menu-image.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateMenuImagesDto } from './create-menu-image.dto';
-
-export class UpdateMenuImageDto extends PartialType(CreateMenuImagesDto) {}
--- src copy/orders/dto/create-order.dto.ts
+++
@@ -1,7 +0,0 @@
-import { OrderItem } from './order-item.dto';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateOrderDto {
-  @ApiProperty()
-  orderItems: OrderItem[];
-}
