b0b3b9346803991557578af2425b7a26b2ea4031
meselghea <122159476+meselghea@users.noreply.github.com>
2023-07-17 10:16:32 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AppModule } from './app.module';
-import { PORT } from './config/config';
-import * as cors from 'cors';
+import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
+import { PORT } from "./config/config";
+import * as cors from "cors";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -13,13 +13,13 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .addBearerAuth()
-    .setTitle('Warteg API')
-    .setDescription('Warteg API documentation')
-    .setVersion('2.0')
-    .addTag('API')
+    .setTitle("Warteg API")
+    .setDescription("Warteg API documentation")
+    .setVersion("2.0")
+    .addTag("API")
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup("api", app, document);
 
   await app.listen(PORT, () => {
     console.log(`Your application is running on http://localhost:${PORT}`);
