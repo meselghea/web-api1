7d301bb22271b0d87a838800a1d4287c18f0d61e
meselghea <122159476+meselghea@users.noreply.github.com>
2023-07-17 07:46:26 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { PORT } from './config/config';
+import * as cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Tambahkan konfigurasi CORS di sini
+  app.use(cors());
 
   const config = new DocumentBuilder()
     .addBearerAuth()
